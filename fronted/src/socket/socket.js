355e9310f50c395940d3b1440e9b887896a76acf
Dinesh verma <vermadinesh9693@gmail.com>
2026-05-29 08:54:34 +0530

    url updates

--- fronted/src/socket/socket.js
+++ fronted/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://studiox-ai-backend.onrender.com");
-const socket = io("http://localhost:5500");
+const socket = io("https://studiox-ai-backend.onrender.com");
+// const socket = io("http://localhost:5500");
 
 
 export default socket;
